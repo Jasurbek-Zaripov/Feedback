53e85fd0b09823aabd8cedb3594a83e369571126
Jasurbek-Zaripov <zaripovjasurbek2001@gmail.com>
2022-02-20 08:01:59 +0500

    changed-suggestion-model

--- src/model/suggestion.model.js
+++ src/model/suggestion.model.js
@@ -5,8 +5,8 @@ const schema = new Schema({
     title: { type: 'string', required: [true, 'Title empty!'], maxlength: [150, 'Title is too long!'], minlength: [2, 'Title is too short!'] },
     description: { type: 'string', required: [true, 'Description empty!'], maxlength: [300, 'Description is too long!'], minlength: [4, 'Description is too short!'] },
     category: { type: 'string' },
-    like: { type: 'number', default: 0, required: true },
-    count_comment: { type: 'number', default: 0, required: true },
+    voices: { type: 'number', default: 0, required: true },
+    count_comments: { type: 'number', default: 0, required: true },
     State: { type: ['Planned', 'InProgress', 'Live'], default: 'Planned' },
     created_at: { type: Schema.Types.Date, default: Date.now }
 });
